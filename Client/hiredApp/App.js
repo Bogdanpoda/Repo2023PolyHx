1a37f4c89ee38ff19635d9f5c4ec7d80e56875ac
Bogdan Podariu <podariubogdan1@gmail.com>
2023-02-04 17:54:21 -0500

    add home page

--- Client/hiredApp/App.js
+++ Client/hiredApp/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './Pages/Login';
+import HomePage from './Pages/Home';
 
 export default function App() {
   return (
-    <LoginPage></LoginPage>
+    <HomePage></HomePage>
   );
 }
 
---
+++ Client/hiredApp/Components/CompanyProfile.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {View, TextInput, Button, Text, StyleSheet} from 'react-native';
+import StarRating from 'react-native-star-rating';
+
+
+export default function companyProfile(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
