fd469457ad191d7c5285fa6ed6065c4873bfa6c4
DanielNicula <113062004+DanielNicula@users.noreply.github.com>
2023-02-04 15:01:05 -0500

    fichier classes initial

---
+++ Server/baseClasses.py
@@ -0,0 +1,44 @@
+class Education:
+    def __init__(self, schoolName: str, field: str, degree: str, startYear: int, graduationYear: int):
+        self.schoolName = schoolName
+        self.field = field
+        self.degree = degree
+        self.startYear = startYear
+        self.graduationYear = graduationYear
+        self.duration = graduationYear - startYear
+
+
+class Company:
+    def __init__(self, name: str, location: str, description: str, jobs: list):
+        self.name = name
+        self.location = location
+        self.description = description
+
+
+class Job:
+    def __init__(self, field: str, company: Company, jobTitle: str, jobDescription: str, salary: int, location: str):
+        self.field = field
+        self.company = company
+        self.jobTitle = jobTitle
+        self.jobDescription = jobDescription
+        self.salary = salary
+        self.location = location
+
+
+class WorkExperience:
+    def __init__(self, job: Job, startYear: int, endYear: int):
+        self.job = job
+        self.startYear = startYear
+        self.endYear = endYear
+        self.duration = endYear - startYear
+
+
+class ContactInfo:
+    def __init__(self, email: str, phoneNumber: str, address: str, zipCode: int, city: str, state: str, Country: str):
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.address = address
+        self.zipCode = zipCode
+        self.city = city
+        self.state = state
+        self.Country = Country
---
+++ Server/classes.py
@@ -0,0 +1,49 @@
+from baseClasses import *
+
+
+class User:
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
+        self.firstName = None
+        self.lastName = None
+        self.ContactInfo = None
+        self.education = None
+        self.currentJob = None
+        self.workExperience = []
+        self.skills = []
+
+    def setFirstName(self, firstName: str):
+        self.firstName = firstName
+
+    def setLastName(self, lastName: str):
+        self.lastName = lastName
+
+    def setContactInfo(self, contactInfo: ContactInfo):
+        self.ContactInfo = contactInfo
+
+    def setEducation(self, education: Education):
+        self.education = education
+
+    def setCurrentJob(self, currentJob: Job):
+        self.currentJob = currentJob
+
+    def addWorkExperience(self, workExperience: WorkExperience):
+        self.workExperience.append(workExperience)
+
+    def addskills(self, skill: str):
+        self.skills.append(skill)
+
+
+class Employer:
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
+        self.company = None
+        self.jobPostings = []
+
+    def setCompany(self, company: Company):
+        self.company = company
+
+    def addJobPosting(self, jobPosting: Job):
+        self.jobPostings.append(jobPosting)
