7333803951d05ee553802003c920e7fc78c9fbe0
DanielNicula <113062004+DanielNicula@users.noreply.github.com>
2023-02-04 19:59:11 -0500

    elements calculs initial

---
+++ Server/fieldMatchingFactors.py
@@ -0,0 +1,52 @@
+AgricultureComparisionDict = {
+    "Agriculture": 1.0, "Architecture": 0.4, "Arts": 0.1, "Biology": 0.6, "Education": 0.3, "SoftwareEngineering": 0.3,
+    "MecanicalEngineering": 0.6, "Finance": 0.4, "Hospitality": 0.1, "Mathematics": 0.2, "Medicine": 0.4, "Music": 0.1,
+    "Logistics": 0.8}
+ArchitectureComparisionDict = {
+    "Agriculture": 0.4, "Architecture": 1.0, "Arts": 0.5, "Biology": 0.1, "Education": 0.4, "SoftwareEngineering": 0.4,
+    "MecanicalEngineering": 0.6, "Finance": 0.4, "Hospitality": 0.4, "Mathematics": 0.7, "Medicine": 0.1, "Music": 0.1,
+    "Logistics": 0.7}
+ArtsComparisionDict = {
+    "Agriculture": 0.1, "Architecture": 0.5, "Arts": 1.0, "Biology": 0.2, "Education": 0.6, "SoftwareEngineering": 0.4,
+    "MecanicalEngineering": 0.2, "Finance": 0.3, "Hospitality": 0.5, "Mathematics": 0.2, "Medicine": 0.1, "Music": 0.7,
+    "Logistics": 0.3}
+BiologyComparisionDict = {
+    "Agriculture": 0.6, "Architecture": 0.1, "Arts": 0.2, "Biology": 1.0, "Education": 0.6, "SoftwareEngineering": 0.4,
+    "MecanicalEngineering": 0.1, "Finance": 0.1, "Hospitality": 0.2, "Mathematics": 0.6, "Medicine": 0.7, "Music": 0.2,
+    "Logistics": 0.4}
+EducationComparisionDict = {
+    "Agriculture": 0.3, "Architecture": 0.4, "Arts": 0.4, "Biology": 0.6, "Education": 1.0, "SoftwareEngineering": 0.5,
+    "MecanicalEngineering": 0.5, "Finance": 0.5, "Hospitality": 0.2, "Mathematics": 0.6, "Medicine": 0.3, "Music": 0.6,
+    "Logistics": 0.6}
+SoftwareEngineeringComparisionDict = {
+    "Agriculture": 0.3, "Architecture": 0.4, "Arts": 0.4, "Biology": 0.4, "Education": 0.5, "SoftwareEngineering": 1.0,
+    "MecanicalEngineering": 0.8, "Finance": 0.7, "Hospitality": 0.6, "Mathematics": 0.6, "Medicine": 0.4, "Music": 0.2,
+    "Logistics": 0.5}
+MecanicalEngineeringComparisionDict = {
+    "Agriculture": 0.6, "Architecture": 0.6, "Arts": 0.2, "Biology": 0.1, "Education": 0.5, "SoftwareEngineering": 0.8,
+    "MecanicalEngineering": 1.0, "Finance": 0.2, "Hospitality": 0.2, "Mathematics": 0.7, "Medicine": 0.2, "Music": 0.1,
+    "Logistics": 0.7}
+FinanceComparisionDict = {
+    "Agriculture": 0.4, "Architecture": 0.4, "Arts": 0.3, "Biology": 0.1, "Education": 0.5, "SoftwareEngineering": 0.7,
+    "MecanicalEngineering": 0.2, "Finance": 1.0, "Hospitality": 0.6, "Mathematics": 0.7, "Medicine": 0.1, "Music": 0.4,
+    "Logistics": 0.7}
+HospitalityComparisionDict = {
+    "Agriculture": 0.1, "Architecture": 0.4, "Arts": 0.5, "Biology": 0.2, "Education": 0.2, "SoftwareEngineering": 0.6,
+    "MecanicalEngineering": 0.2, "Finance": 0.6, "Hospitality": 1.0, "Mathematics": 0.1, "Medicine": 0.2, "Music": 0.4,
+    "Logistics": 0.6}
+MathematicsComparisionDict = {
+    "Agriculture": 0.2, "Architecture": 0.7, "Arts": 0.2, "Biology": 0.6, "Education": 0.6, "SoftwareEngineering": 0.6,
+    "MecanicalEngineering": 0.7, "Finance": 0.7, "Hospitality": 0.1, "Mathematics": 1.0, "Medicine": 0.2, "Music": 0.1,
+    "Logistics": 0.7}
+MedicineComparisionDict = {
+    "Agriculture": 0.4, "Architecture": 0.1, "Arts": 0.1, "Biology": 0.7, "Education": 0.3, "SoftwareEngineering": 0.4,
+    "MecanicalEngineering": 0.2, "Finance": 0.1, "Hospitality": 0.2, "Mathematics": 0.2, "Medicine": 1.0, "Music": 0.1,
+    "Logistics": 0.3}
+MusicComparisionDict = {
+    "Agriculture": 0.1, "Architecture": 0.1, "Arts": 0.7, "Biology": 0.2, "Education": 0.6, "SoftwareEngineering": 0.2,
+    "MecanicalEngineering": 0.1, "Finance": 0.4, "Hospitality": 0.4, "Mathematics": 0.1, "Medicine": 0.1, "Music": 1.0,
+    "Logistics": 0.1}
+LogisticsComparisionDict = {
+    "Agriculture": 0.8, "Architecture": 0.7, "Arts": 0.3, "Biology": 0.4, "Education": 0.6, "SoftwareEngineering": 0.5,
+    "MecanicalEngineering": 0.7, "Finance": 0.7, "Hospitality": 0.6, "Mathematics": 0.7, "Medicine": 0.3, "Music": 0.1,
+    "Logistics": 1.0}
